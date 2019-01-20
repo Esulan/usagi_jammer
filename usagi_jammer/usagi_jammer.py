8050d7b80c78f25c7d822b1cba9b5f901413d1ff
Naruse Maeno <sevenshallows02@gmail.com>
2019-01-21 01:06:01 +0900

    [fix]写真の改善

--- usagi_jammer/usagi_jammer.py
+++ usagi_jammer/usagi_jammer.py
@@ -1,80 +1,47 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 16,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Choose 1 picture\n",
-      "a\n",
-      "Nothing picture\n"
-     ]
-    }
-   ],
-   "source": [
-    "import cv2\n",
-    "import matplotlib.pyplot as plt\n",
-    "\n",
-    "def usagi(img, rect):\n",
-    "    (x1, y1, x2, y2) = rect\n",
-    "    w = x2 - x1\n",
-    "    h = y2 - y1\n",
-    "    newimg = cv2.imread(\"usagi.png\")\n",
-    "    i_rect = img[y1:y2, x1:x2]\n",
-    "    \n",
-    "    i_mos = cv2.resize(newimg, (w, h), interpolation=cv2.INTER_AREA)\n",
-    "    \n",
-    "    img2 = img.copy()\n",
-    "    img2[y1:y2, x1:x2] = i_mos\n",
-    "    return img2\n",
-    "\n",
-    "cascade_file = \"haarcascade_frontalface_alt.xml\"\n",
-    "cascade = cv2.CascadeClassifier(cascade_file)\n",
-    "\n",
-    "print(\"Choose 1 picture\")\n",
-    "str = input()\n",
-    "img = cv2.imread(str)\n",
-    "if img is not None:\n",
-    "    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)\n",
-    "\n",
-    "    face_list = cascade.detectMultiScale(img_gray, minSize=(10, 10))\n",
-    "    if len(face_list) == 0:\n",
-    "        quit()\n",
-    "\n",
-    "    for (x, y, w, h) in face_list:\n",
-    "        img = usagi(img, (x, y, x + w, y + h))\n",
-    "\n",
-    "    cv2.imwrite(\"autousagi.png\", img)\n",
-    "    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))\n",
-    "    plt.show()\n",
-    "else:\n",
-    "    print(\"Nothing picture\")"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.0"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+def usagi(img, rect):
+    (x1, y1, x2, y2) = rect
+    w = x2 - x1
+    h = y2 - y1
+    
+    newimg = Image.open("usagi.png")
+    newimg2 = newimg.copy()
+    i_mos = newimg2.resize((w, h))
+    
+    img.paste(i_mos, (x1, y1), i_mos.split()[3])
+    return img
+
+cascade_file = "haarcascade_frontalface_alt.xml"
+cascade = cv2.CascadeClassifier(cascade_file)
+
+print("Choose 1 picture")
+str = input()
+img = cv2.imread(str)
+if img is not None:
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    face_list = cascade.detectMultiScale(img_gray, minSize=(10, 10))
+    if len(face_list) == 0:
+        quit()
+
+    pil_img = Image.open(str)
+    p_img = pil_img.copy()
+    
+    
+    for (x, y, w, h) in face_list:
+        p_img = usagi(p_img, (x, y, x + w, y + h))
+
+    plt.imshow(np.asarray(p_img))
+    plt.show()
+    
+    try:
+        p_img.save("autousagi_" + str, "JPEG")
+    except AttributeError:
+        print("Couldn't save image {}".format("autousagi_" + str))
+        
+else:
+    print("Nothing picture")
